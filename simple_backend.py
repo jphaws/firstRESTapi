ed57deb04bd61e19812b1d54753a8946272f2a6b
jphaws <jphaws@calpoly.edu>
2021-01-11 15:19:08 -0800

    5. Using the POST method

--- simple_backend.py
+++ simple_backend.py
@@ -14,6 +14,24 @@ def get_user(id):
         return ({})
     return users
 
+@app.route('/users', methods=['GET', 'POST'])
+def get_users():
+    if request.method == 'GET':
+        search_username = request.args.get('name')
+        if search_username :
+            subdict = {'users_list' : []}
+            for user in users['users_list']:
+                if user['name'] == search_username:
+                    subdict['users_list'].append(user)
+            return subdict
+        return users
+    elif request.method == 'POST':
+        userToAdd = request.get_json()
+        users['users_list'].append(userToAdd)
+        resp = jsonify(success=True)
+        #resp.status_code = 200 #optionally, you can always set a response code. 
+        # 200 is the default code for a normal response
+        return resp
 
 users = {
     'users_list' :
